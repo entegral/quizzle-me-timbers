80a57770874f49ef93a703950bfa03a2eb211003
Daniel Bennett <daniel@>
2019-02-04 09:38:50 -0800

    add host and team routes and splash page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HostComponent } from './host/host.component';
+import { TeamComponent } from './team/team.component';
+import { routing } from './app.routing';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HostComponent,
+    TeamComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
